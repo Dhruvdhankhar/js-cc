45ca9bf4c6fa65291bb423778cca7b09d8f8494a
Dhruv <dhruvdhkh@gmail.com>
2024-12-24 19:05:26 +0000

    01-basics/03

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -1,1 +1,24 @@
-console.log("Will Continue");
+let number = 33;
+let string = String(number)
+
+//console.log(typeof string)
+//console.log(string);
+
+let boolean = true;
+let number1 = Number(boolean);
+console.log(typeof number1)
+console.log(number1);
+
+// some results of conversion is as follow
+
+// FOR STRING/BOOLEAN TO NUMBER
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// STRING/NUMBER TO BOOLEAN 
+
+// 1 => true; 0 => false
+// "" => false (empty string)
+// "hitesh" => true
