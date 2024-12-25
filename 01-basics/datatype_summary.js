5ec67ac658bd54d13e7f89c09b5092ce9ee731af
Dhruv <dhruvdhkh@gmail.com>
2024-12-25 16:33:23 +0000

    heap/stack memory

--- 01-basics/datatype_summary.js
+++ 01-basics/datatype_summary.js
@@ -31,3 +31,28 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+/* Heap and stack memory
+Stack --> all primitive type uses stack memory means when recall it uses a copy of primitive not directly use it
+Heap --> all non-primitive/reference datatype uses heap memory means will use direct refernce. 
+ */
+
+let user1 = "Dhruv";
+
+let user2 = user1;
+
+user2 = "Dhankhar";
+
+console.log(user1); // => Dhruv, as it uses stack memory(copy memory), change user2 doesnt change it
+console.log(user2); // => Dhankhar 
+
+let type1 = {
+    email: "xyz@gamil.com",
+    upi: "type1@ybl"
+}
+let type2 = type1;
+
+type2.email = "abc@gmail.com"
+
+console.log(type1.email); // => since it use heap memory, change reflects in each other.
+console.log(type2.email);
