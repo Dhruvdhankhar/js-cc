7598955f51434bdca89bc65db1bae2bd247aaa35
Dhruv Dhankhar <dhruvdhkh@gmail.com>
2024-12-26 00:56:59 +0530

    added comment

--- 01-basics/05_string.js
+++ 01-basics/05_string.js
@@ -2,3 +2,5 @@ let nam = "Dhruv"
 let repoCount = 5
 
 console.log(`the person ${nam} has created ${repoCount} repositories`);
+// ${} this is called string interpolation for better readability and to perform any operation eg. nam.lenght, etc
+
