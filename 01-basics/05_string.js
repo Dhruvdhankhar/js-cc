b41decdc0353975b33e6bd87f70e6d3d4c8369b5
Dhruv Dhankhar <dhruvdhkh@gmail.com>
2024-12-27 02:43:35 +0530

    initial commit

--- 01-basics/05_string.js
+++ 01-basics/05_string.js
@@ -4,3 +4,8 @@ let repoCount = 5
 console.log(`the person ${nam} has created ${repoCount} repositories`);
 // ${} this is called string interpolation for better readability and to perform any operation eg. nam.lenght, etc
 
+const gameName = "chess";  // => chess only
+const gameName1 = new String("chess"); // String with new returns a string wrapper object. => [String: 'chess']
+
+console.log(gameName);
+console.log(gameName1);
